54303e01f6d8bbb1499dd03418d27e23c6d1d4b4
Maksim Titiaiev <gipsmas@gmail.com>
2018-03-25 21:50:26 +0300

    отрефакторил js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,56 @@
 "use strict";
 window.onload = function () {
 
-    // добавить кнопки не пустым элементам списка и повесить на них события клика
-    var problemsList = document.querySelectorAll("#problems-list > li");
-    for (var i = 0; i < problemsList.length; i++) {
-        if (problemsList[i].childNodes.length > 0) {
+    var problemsList = Array.from( document.querySelectorAll("#problems-list > li") );
+    // добавить кнопки не пустым элементам списка
+    problemsList.forEach( function (problem) {
+
+        if (problem.childNodes.length > 0) {
             var btn = document.createElement("button");
             btn.innerText = "решить";
-            problemsList[i].appendChild(btn);
+            problem.appendChild(btn);
         }
-    }
-    var btns = document.querySelectorAll('#problems-list button');
-    for (var j = 0; j < btns.length; j++) {
+    })
+        
 
-        btns[j].addEventListener('click', function (e) {
-            showAnswer(e.target.parentNode, e.target.parentNode.getAttribute('data-id')); console.log(e.target.parentNode.getAttribute('data-id'));
+    // повесить на кнопки события клика
+    document.getElementById('problems-list').addEventListener('click', function (e) {
+
+        if(e.target.nodeName === 'BUTTON') {
+            calculateAndDisplayAnswer(e.target.parentNode, e.target.parentNode.getAttribute('data-id'));
             e.target.parentNode.removeChild(e.target);
-        });
-    }
+        }
+
+    }, true);
 
 
 
     // показать ответ и код
-    function showAnswer(target, problemsNumber) {
+    function calculateAndDisplayAnswer(target, problemsNumber) {
 
         var worker = new Worker('js/workers/' + problemsNumber + '.js');
+        var answerSection = document.createElement("p");
+        var code = document.createElement("pre");
+        var wraper = document.createElement('div');
+
+        answerSection.classList.add('answer-section');
+        code.classList.add('code-section');
+        
+        wraper.appendChild(answerSection);
+        wraper.appendChild(code);
 
         worker.postMessage('');
+
         worker.onmessage = function (response) {
 
-            var answerSection = document.createElement("p");
-            answerSection.className += ' answer-section';
             answerSection.innerText = "Ответ: " + response.data.answer;
-            target.appendChild(answerSection);
-
-            var code = document.createElement("pre");
-            code.className += ' code-section';
             code.innerHTML = response.data.code;
-            target.appendChild(code);
+            target.appendChild(wraper);
         }
 
         worker.onerror = function () {
-            console.log('файла с именем '+ problemsNumber +'.js не существует.');
+            wraper.innerHTML = '<p class="answer-section">файла с именем '+ problemsNumber +'.js не существует.</p>'
+            target.appendChild(wraper);
         }
     }
 }
