87977af5a7840bd884a707c962ba81d32a8ecbfa
Maksim Titiaiev <gipsmas@gmail.com>
2018-03-26 22:12:25 +0300

    протестировал новый способ вычисления полиндрома

--- js/workers/problem4.js
+++ js/workers/problem4.js
@@ -1,16 +1,17 @@
 'use strict'
-
+// answer 906609
 var calc = function() {
     var polindrom = 0;
 
         console.time('время выполнения скрипта');
         for (var i = 999; i > 99; i--) {
             for (var j = 999; j > 99; j--) {
-                if (isPolindrom(i * j) && i * j > polindrom) {
+                var currentMult = ( i * j );
+                if (isPolindrom(currentMult) && currentMult > polindrom) {
                     polindrom = i * j;
-                    console.log('i: ' + i);
-                    console.log('j: ' + j);
-                    console.log('-------------');
+                    // console.log('i: ' + i);
+                    // console.log('j: ' + j);
+                    // console.log('-------------');
                 }
             }
         }
@@ -32,6 +33,11 @@ var calc = function() {
             } else {
                 return false
             }
+
+            // это решение гараздо короче, но медленее в 5 раз
+            // var checkingValue = num.toString();
+            // var reverseValue = checkingValue.split('').reverse().join('');
+            // return checkingValue === reverseValue;
         }
 
         return polindrom;
