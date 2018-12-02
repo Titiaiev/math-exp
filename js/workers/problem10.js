44da76448746a6c93aa30b84bc98e1c3e3c518b4
Maksim Titiaiev <gipsmas@gmail.com>
2018-12-02 12:15:20 +0200

    отформатировал в соответствии с линтером

--- js/workers/problem10.js
+++ js/workers/problem10.js
@@ -1,44 +1,42 @@
-'use strict'
+/* eslint-disable func-names */
 
-var calc = function() {
-    console.time('время выполнения: ');
-        var sum = 0;
 
-        function eratosfen(n) {
-            var arr = Array(n);
-            var p = 2;
+const calc = function () {
+  console.time('время выполнения: ');
+  let sum = 0;
 
-            arr.fill(true);
-            arr[0] = false;
-            arr[1] = false;
+  function eratosfen(n) {
+    const arr = Array(n);
+    let p = 2;
 
-            while (p < n && p > 0) {
+    arr.fill(true);
+    arr[0] = false;
+    arr[1] = false;
 
-                for (var i = 2 * p; i < n; i += p) {
-                    arr[i] = false;
-                }
+    while (p < n && p > 0) {
+      for (let i = 2 * p; i < n; i += p) {
+        arr[i] = false;
+      }
 
-                p = arr.indexOf(true, (p + 1));
-            }
+      p = arr.indexOf(true, (p + 1));
+    }
 
-            return arr;
-        }
+    return arr;
+  }
 
-        var list = eratosfen(2e6); // 2000000
+  const list = eratosfen(2e6); // 2000000
 
-        for (var j = 2; j < list.length; j++) {
-            if (list[j]) sum += j;
-        }
+  for (let j = 2; j < list.length; j++) {
+    if (list[j]) sum += j;
+  }
 
-        console.timeEnd('время выполнения: ');
-        return sum;
-}
+  console.timeEnd('время выполнения: ');
+  return sum;
+};
 
-addEventListener('message', function () {
-
-    postMessage({
-        answer: calc(),
-        code: calc.toString()
-    });
-
-})
+addEventListener('message', () => {
+  postMessage({
+    answer: calc(),
+    code: calc.toString(),
+  });
+});
